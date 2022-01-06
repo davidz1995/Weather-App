53abc0d4d63203b12c84d2cce3f45c32f9e20c06
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-01-06 13:30:49 -0300

    Terminada ruta current

--- backend/routers/current.js
+++ backend/routers/current.js
@@ -1,9 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios')
+const axios = require('axios');
 
 router.get(`/`,async (req, res) =>{
+    const currentUbicationURL = process.env.CURRENT_LOCATION;
+    const apiKey = process.env.API_KEY;
     
+    try{
+        let currentLocation = await axios.get(`${currentUbicationURL}`);
+        let currentCity = await currentLocation.data.city;
+        let currentLocationWeather = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}`);
+        let response = {
+            locationData:currentLocation.data,
+            currentCityWeather: currentLocationWeather.data
+        }
+        res.status(200).send(response)
+    }catch(error){
+            res.status(400).send(error.message)
+    }
+})
+
+router.get(`/:city`,async (req, res) =>{
+    let city = req.params.city;
+    const apiKey = process.env.API_KEY;
+    try{
+        let optionalCityWeather = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`);
+        res.status(200).send(optionalCityWeather.data)
+    }catch(error){
+        res.status(400).send(error.message)
+    }
 })
 
 module.exports = router
--- backend/routers/forecast.js
+++ backend/routers/forecast.js
@@ -2,11 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get(`/`,async (req, res) =>{
-    let currentCoordinate = {
-        lat:currentLocation.data.lat,
-        lon:currentLocation.data.lon
-    }
-    let currentUbicationData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${currentCoordinate.lat}&lon=${currentCoordinate.lon}&appid=${apiKey}`)
+    
 })
 
 module.exports = router
